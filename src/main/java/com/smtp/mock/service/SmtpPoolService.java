0be1b5fa54d7567b703c7d272d66d5274887b882
irlu <irlu@3169R72.genband.com>
2018-09-12 16:03:23 -0400

    Smtp connection pooling with open connection

--- src/main/java/com/smtp/mock/service/SmtpPoolService.java
+++ src/main/java/com/smtp/mock/service/SmtpPoolService.java
@@ -12,83 +12,84 @@ import javax.mail.Session;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMessage.RecipientType;
 
-import org.nlab.smtp.pool.SmtpConnectionPool;
-import org.nlab.smtp.transport.connection.ClosableSmtpConnection;
-import org.nlab.smtp.transport.factory.SmtpConnectionFactory;
-import org.nlab.smtp.transport.factory.SmtpConnectionFactoryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.smtp.mock.config.SmtpGmailConfig;
+import com.smtp.mock.connection.ClosableSmtpConnection;
 import com.smtp.mock.entities.EmailMessage;
+import com.smtp.mock.factory.SmtpConnectionFactory;
+import com.smtp.mock.factory.SmtpConnectionFactoryBuilder;
+import com.smtp.mock.pool.SmtpConnectionPool;
 
 @Service
 public class SmtpPoolService {
-	@Autowired
-	private SmtpGmailConfig smtpConfig;
+  @Autowired
+  private SmtpGmailConfig smtpConfig;
 
-	private SmtpConnectionPool connectionPool;
-	private SmtpConnectionFactory factory;
+  private SmtpConnectionPool connectionPool;
+  private SmtpConnectionFactory factory;
 
-	@PostConstruct
-	private void init() {
-		this.factory = SmtpConnectionFactoryBuilder.newSmtpBuilder().session(setEmailSession()).build();
-		this.connectionPool = new SmtpConnectionPool(factory);
-	}
+  @PostConstruct
+  private void init() {
+    this.factory = SmtpConnectionFactoryBuilder.newSmtpBuilder().session(setEmailSession()).build();
+    this.connectionPool = new SmtpConnectionPool(factory);
+    connectionPool.init();
+  }
 
-	private Session setEmailSession() {
-		Properties props = new Properties();
-		props.put("mail.smtp.host", smtpConfig.getSmtpHost());
-		props.put("mail.smtp.port", smtpConfig.getSmtpPort());
-		props.put("mail.transport.protocol", "smtp");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.debug", "true");
+  private Session setEmailSession() {
+    Properties props = new Properties();
+    props.put("mail.smtp.host", smtpConfig.getSmtpHost());
+    props.put("mail.smtp.port", smtpConfig.getSmtpPort());
+    props.put("mail.transport.protocol", "smtp");
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.starttls.enable", "true");
+    props.put("mail.debug", "true");
 
-		Authenticator authenticator = new Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(smtpConfig.getUserName(), smtpConfig.getPassword());
-			}
-		};
+    Authenticator authenticator = new Authenticator() {
+      protected PasswordAuthentication getPasswordAuthentication() {
+        return new PasswordAuthentication(smtpConfig.getUserName(), smtpConfig.getPassword());
+      }
+    };
 
-		Session session = Session.getInstance(props, authenticator);
-		return session;
-	}
+    Session session = Session.getInstance(props, authenticator);
+    return session;
+  }
 
-	public EmailMessage send(String userId, EmailMessage emailMessage) {
-		if (emailMessage == null) {
-			System.err.println("No requested message!!");
-			return null;
-		}
+  public EmailMessage send(String userId, EmailMessage emailMessage) {
+    if (emailMessage == null) {
+      System.err.println("No requested message!!");
+      return null;
+    }
 
-		List<String> addresses = emailMessage.getAddress();
+    List<String> addresses = emailMessage.getAddress();
 
-		try (ClosableSmtpConnection connection = connectionPool.borrowObject()) {
-			MimeMessage mimeMessage = new MimeMessage(connection.getSession());
-			if (addresses.size() == 1) {
+    try (ClosableSmtpConnection connection = connectionPool.borrowObject()) {
+      MimeMessage mimeMessage = new MimeMessage(connection.getSession());
+      if (addresses.size() == 1) {
 
-				mimeMessage.setRecipients(RecipientType.TO, addresses.get(0));
+        mimeMessage.setRecipients(RecipientType.TO, addresses.get(0));
 
-			} else {
-				String dest = addresses.stream().collect(Collectors.joining(","));
-				mimeMessage.setRecipients(RecipientType.TO, dest);
-			}
-			mimeMessage.setSubject(emailMessage.getSubject());
-			mimeMessage.setText(emailMessage.getMessage());
+      } else {
+        String dest = addresses.stream().collect(Collectors.joining(","));
+        mimeMessage.setRecipients(RecipientType.TO, dest);
+      }
+      mimeMessage.setSubject(emailMessage.getSubject());
+      mimeMessage.setText(emailMessage.getMessage());
 
-			System.err.println("MimeMessage prepared: " + mimeMessage.toString());
+      System.err.println("MimeMessage prepared: " + mimeMessage.toString());
 
-			connection.sendMessage(mimeMessage);
-			System.err.println("Message sent!");
+      connection.sendMessage(mimeMessage);
+      System.err.println("Message sent!");
 
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			System.err.println("Connection is null");
-			e.printStackTrace();
-		}
-		String messageId = UUID.randomUUID().toString();
-		String resourceURL = String.format("/cpaas/email/%s/messages/%s", userId, messageId);
-		emailMessage.setResourceURL(resourceURL);
-		return emailMessage;
-	}
+    } catch (Exception e) {
+      // TODO Auto-generated catch block
+      System.err.println("Connection is null");
+      e.printStackTrace();
+    }
+    String messageId = UUID.randomUUID().toString();
+    String resourceURL = String.format("/cpaas/email/%s/messages/%s", userId, messageId);
+    emailMessage.setResourceURL(resourceURL);
+    return emailMessage;
+  }
 }
